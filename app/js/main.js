92770d51a1c6f766ab1ae81311d881ab7786883d
timur-webdev <timi.biz@bk.ru>
2023-06-07 21:54:43 +0400

    fix: calc

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
-const length = document.querySelector('#length') // input Длина
-const width = document.querySelector('#width') // input Ширина
+const lengthData = document.querySelector('#length') // input Длина
+const widthData = document.querySelector('#width') // input Ширина
 const square = document.querySelector('#square') // input квадратный метр
 const sumOut = document.querySelector('.out-wrap__total') // Вывод div Сумма
 const partsOutSum = document.querySelector('#parts-sum') // Вывод Цена + комплектующие
@@ -7,24 +7,37 @@ const montageOutSum = document.querySelector('#montage') // Вывод Цена 
 const squarePrice = 454.25 // Цена за квадрат Байкал и Ритейл
 const partsPrice = 242 // Цена Комплектующие кв.м
 const montagePrice = 450 // Цена за монтаж кв.м
-let valueSquare // Количество квадратных метров
+const button = document.querySelector('.buttons__button') // Кнопка
 let sum // Сумма 
 
-const button = document.querySelector('.buttons__button') // Кнопка
-
 button.addEventListener('click', (e) => {
-  if (square.value != 0 || square.value.length != 0) {
-    valueSquare = square.value
-  } 
+  let l = +lengthData?.value
+  let w = +widthData?.value
+  let s = +square?.value
 
-  if (square.value.length === 0 || square.value == 0) {
-    valueSquare = length.value * width.value
-    square.value = length.value * width.value
+  let valueSquare = s > 0 ? s : l + w // Количество квадратных метров
+
+  console.log(valueSquare)
+  if (s > 0 || l * w === s) {
+    sum = valueSquare * squarePrice 
+    sumOut.textContent = `${sum} ₽`
+    partsOutSum.textContent = `${sum + partsPrice * valueSquare} ₽`
+    montageOutSum.textContent = `${sum + partsPrice * valueSquare + montagePrice * valueSquare} ₽`
   }
 
-  sum = valueSquare * squarePrice
-  sumOut.textContent = `${sum} ₽`
-  partsOutSum.textContent = `${sum + partsPrice * valueSquare} ₽`
-  montageOutSum.textContent = `${sum + partsPrice * valueSquare + montagePrice * valueSquare} ₽`
-  console.log(valueSquare, square.value)
+  if (s > 0 && l * w != s) {
+    lengthData.value = 0
+    widthData.value = 0
+    sum = valueSquare * squarePrice
+    sumOut.textContent = `${sum} ₽`
+    partsOutSum.textContent = `${sum + partsPrice * valueSquare} ₽`
+    montageOutSum.textContent = `${sum + partsPrice * valueSquare + montagePrice * valueSquare} ₽`
+  }
+
+  if ( s <= 0 ) {
+    sum = valueSquare * squarePrice 
+    sumOut.textContent = `${sum} ₽`
+    partsOutSum.textContent = `${sum + partsPrice * valueSquare} ₽`
+    montageOutSum.textContent = `${sum + partsPrice * valueSquare + montagePrice * valueSquare} ₽`
+  }
 })
